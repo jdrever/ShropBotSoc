bc8fad59f4c68810b46c945de6396e29db5c36c4
Joe J Collins <joejcollins@users.noreply.github.com>
2018-11-15 15:02:19 +0000

    Set up notes and config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'texas';
-$db['default']['password'] = 'flood';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'password';
 $db['default']['database'] = 'shropsdb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
