4ab6f7f69b52e1888ff3be66b0b6ccaa9967406f
Joe J Collins <joejcollins@users.noreply.github.com>
2018-11-22 12:09:04 +0000

    Dev config

---
+++ application/config/development/config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+*/
+
+$config['base_url'] = 'http://localhost/botanical_records';
+//$config['base_url'] = 'http://localhost:8080';
