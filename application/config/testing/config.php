8f7005959164acd54d0e3f707d5abc9b6c3cacab
Joe J Collins <joejcollins@users.noreply.github.com>
2019-09-07 12:59:23 +0100

    Changed back to captain-blue

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -7,4 +7,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 */
 
-$config['base_url'] = 'https://records.shropshirebotany.org';
+$config['base_url'] = 'https://captain-blue.azurewebsites.net';
