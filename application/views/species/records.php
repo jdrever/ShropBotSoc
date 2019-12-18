45967075cc22e6ec65510eefc5dade132d88cc3e
Joe J Collins <joejcollins@users.noreply.github.com>
2019-12-18 22:09:52 +0000

    Crude download of data

--- application/views/species/records.php
+++ application/views/species/records.php
@@ -1,5 +1,5 @@
 <h2><?=urldecode($speciesName)?></h2>
-
+<p><a href="https://records-ws.nbnatlas.org/occurrences/index/download?q=data_resource_uid:dr782&fq=taxon_name:<?=$speciesName?>&reasonTypeId=11&fileType=csv">Download this data</a></p>
 <?php if (isset($records)):?>
     <table class="table">
         <thead><tr><th>Site</th><th>Location</th><th>Collector</th><th>Year</th></tr></thead>
