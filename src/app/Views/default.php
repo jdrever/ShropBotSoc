913e96c27eb404a0b6b921f33c06a1b4bd5069c4
Joe J Collins <joejcollins@users.noreply.github.com>
2020-05-31 20:41:12 +0100

    Using Blogger style sheet

--- src/app/Views/default.php
+++ src/app/Views/default.php
@@ -5,18 +5,18 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta content='Promoting the enjoyment, understanding and conservation of the flora of Shropshire' name='description'/>
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <!-- Custom styles for this template -->
-  <link href="<?php echo base_url('assets/css/sticky-footer.css'); ?>" rel="stylesheet">
+  <link href="/css/from-blogger.css" rel="stylesheet">
   <title>
     <?=$title?>
   </title>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <a class="navbar-brand" href="<?php echo base_url('/'); ?>">Botanical Records</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main" aria-controls="navbarSupportedContent"
       aria-expanded="false" aria-label="Toggle navigation">
@@ -25,13 +25,7 @@
     <div class="navbar-collapse collapse" id="navbar-main">
       <ul class="nav navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url('/species/'); ?>">Species</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url('/sites/'); ?>">Sites</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo base_url('/tetrads/'); ?>">Tetrads</a>
+          <a class="nav-link" href="/species">Species</a>
         </li>
       </ul>
       <ul class="nav navbar-nav ml-auto">
@@ -49,22 +43,17 @@
     </button>
   </div>
   <?php endif;?>
-  <div class="container">
+  <div class="container content-inner">
     <?= $this->renderSection('content') ?>
   </div>
   <footer class="footer">
     <div class="container">
-      <span class="text-muted">Shropshire Botanical Data</span>
+      <span class="text-muted">Shropshire Botanical Society Data</span>
     </div>
   </footer>
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-    crossorigin="anonymous"></script>
-  <script src="<?php echo base_url('assets/js/shrops.js'); ?>" type="javascipt"></script>
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- src/app/Views/welcome.php
+++ src/app/Views/welcome.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Shropshire Flora</title>
-	<meta name="description" content="Shropshire Flora based on NBN data">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
-</head>
-<body>
-
-<section>
-
-	<h1>Not available yet.</h1>
-
-	<?php 
-	
-	echo "Hello World"; 
-	
-	
-	?>
-
-</section>
-
-</body>
-</html>
+<?= $this->extend('default') ?>
+<?= $this->section('content') ?>
+<h1>Testing</h1>
+<?= $this->endSection() ?>
