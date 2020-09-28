d9765fe14b517dd67fcb15ffb1b461d6dd0a6d83
Joe J Collins <joejcollins@users.noreply.github.com>
2020-09-28 01:08:10 +0100

    GAE environment only

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -18,7 +18,7 @@ class Home extends BaseController
 		$environment_array = getenv();
 		$gae_environment_array = [];
 		foreach($environment_array AS $key => $val)
-			//if(strpos(" ".$key, "GAE_") == 1)    
+			if(strpos(" ".$key, "GAE_") == 1)    
 				array_push($gae_environment_array, array($key, $val)); 
 		return $gae_environment_array;
 	}
