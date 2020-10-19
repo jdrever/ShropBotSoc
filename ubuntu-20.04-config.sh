fb95a511311f031ab2a1c4caf15797bf0a5f8333
Joe Collins <joejcollins@gmail.com>
2020-10-19 09:28:43 +0100

    Alternative way to run the playbook

--- ubuntu-20.04-config.sh
+++ ubuntu-20.04-config.sh
@@ -2,4 +2,4 @@
 # Install Ansible
 sudo apt update && sudo apt -y install ansible
 # Run the Ansible play-book to install the rest
-sudo ansible-playbook ubuntu-20.04-playbook.yml
+sudo ansible-playbook ubuntu-20.04-playbook.yml --connection=local --inventory 127.0.0.1,
