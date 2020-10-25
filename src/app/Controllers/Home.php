f05ad5a22fe5e1ba0ecb213d26216725e9999907
Joe Collins <j.collins@zengenti.com>
2020-10-25 14:46:30 +0000

    Welcome message

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -1,13 +1,15 @@
 <?php namespace App\Controllers;
 
-helper('gae');
-
 class Home extends BaseController
 {
 	public function index()
 	{
 		$data['title'] = 'Testing';
-		$data['gae_environment'] = get_gae_environment();
+		$data['content'] = 'Not on GAE, caching in place.';
+		if (getenv('IS_GAE') == "TRUE")
+		{
+			$data['content'] = 'Running on GAE Standard Environment (no caching).';
+		}
 		echo view('welcome', $data);
 	}
 
--- src/app/Views/welcome.php
+++ src/app/Views/welcome.php
@@ -2,12 +2,5 @@
 <?= $this->section('content') ?>
 <h1>Test Application</h1>
 <p>For test and evaluation.</p>
-<?php 
-if (!empty($gae_environment))
-{
-  $table = new \CodeIgniter\View\Table(); 
-  $table->setHeading("Key", "Value");
-  echo $table->generate($gae_environment);
-}
-?>
+<p><?php echo $content ?></p>
 <?= $this->endSection() ?>
