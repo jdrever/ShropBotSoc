694f6b4a3cce6833991d2faf9019b7063c4571e4
Charles Roper <charles@roper.im>
2021-03-10 19:41:37 +0000

    Code style: fix variable names in Libraries

--- src/app/Libraries/NbnRecords.php
+++ src/app/Libraries/NbnRecords.php
@@ -3,11 +3,10 @@
 /**
  * Facade for the NBN records end point
  */
-
 class NbnRecords
 {
-	const BASE_URL            = 'https://records-ws.nbnatlas.org/';
-	public $data_resource_uid = 'dr782'; //  782=The SEDN data set.  Use 1323 for Worcestershire data if Shrop data not available
+	const BASE_URL          = 'https://records-ws.nbnatlas.org/';
+	public $dataResourceUid = 'dr1323'; //  782=The SEDN data set.  Use 1323 for Worcestershire data if Shrop data not available
 	public $facets;
 	public $fsort;
 	public $path     = '';
@@ -26,13 +25,13 @@ class NbnRecords
 	 */
 	function getQueryString($url)
 	{
-		$query_string  = $url . '?';
-		$query_string .= 'q=data_resource_uid:' . $this->data_resource_uid . '&';
-		$query_string .= 'fq=' . implode('%20AND%20', $this->filter_query_parameters) . '&';
-		$query_string .= 'facets=' . $this->facets . '&';
-		$query_string .= 'sort=' . $this->sort . '&';
-		$query_string .= 'fsort=' . $this->fsort . '&';
-		return $query_string;
+		$queryString  = $url . '?';
+		$queryString .= 'q=data_resource_uid:' . $this->dataResourceUid . '&';
+		$queryString .= 'fq=' . implode('%20AND%20', $this->filterQueryParameters) . '&';
+		$queryString .= 'facets=' . $this->facets . '&';
+		$queryString .= 'sort=' . $this->sort . '&';
+		$queryString .= 'fsort=' . $this->fsort . '&';
+		return $queryString;
 	}
 
 	/**
@@ -50,9 +49,9 @@ class NbnRecords
 	 */
 	function getPagingQueryString()
 	{
-		$query_string  = $this->getQueryString($this::BASE_URL . $this->path);
-		$query_string .= 'pageSize=' . $this->pageSize;
-		return $query_string;
+		$queryString  = $this->getQueryString($this::BASE_URL . $this->path);
+		$queryString .= 'pageSize=' . $this->pageSize;
+		return $queryString;
 	}
 
 	/**
@@ -62,24 +61,24 @@ class NbnRecords
 	 */
 	function getDownloadQueryString()
 	{
-		$query_string  = $this->getQueryString($this::BASE_URL . 'occurrences/index/download');
-		$query_string .= '&reasonTypeId=11&fileType=csv';
-		return $query_string;
+		$queryString  = $this->getQueryString($this::BASE_URL . 'occurrences/index/download');
+		$queryString .= '&reasonTypeId=11&fileType=csv';
+		return $queryString;
 	}
 
 	/**
 	 * @var string[]
 	 */
-	protected $filter_query_parameters = [];
+	protected $filterQueryParameters = [];
 
 	/**
 	 * Adds to the internal list of filter query parameters
 	 *
 	 * @return $this
 	 */
-	public function add($filter_query_parameter)
+	public function add($filterQueryParameter)
 	{
-		$this->filter_query_parameters[] = $filter_query_parameter;
+		$this->filterQueryParameters[] = $filterQueryParameter;
 		return $this;
 	}
 
