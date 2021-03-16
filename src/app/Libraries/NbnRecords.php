1ef4aa8ded0219ba4c53a3e716e327ce1296a908
Charles Roper <charles@roper.im>
2021-03-16 00:06:24 +0000

    Code style: add docblocks and type hints

--- src/app/Libraries/NbnRecords.php
+++ src/app/Libraries/NbnRecords.php
@@ -2,19 +2,82 @@
 
 /**
  * Facade for the NBN records end point
+ *
+ * See the NBN Atlas Query Primer for details about using the API
+ * https://docs.google.com/document/d/1FiVasGGZ3kRPnu5347GPAef7Tr5LvvghCS6x82xnfu4/edit
+ *
+ * @package Libraries
+ * @author  Careful Digital <hello@careful.digital>
+ * @license https://www.shropshirebotany.org.uk/ Shropshire Botanical Society
  */
 
 class NbnRecords
 {
-	const BASE_URL           = 'https://records-ws.nbnatlas.org/';
-	private $dataResourceUid = 'dr1323'; //  782=The SEDN data set.  Use 1323 for Worcestershire data if Shrop data not available
-	private $path     = '';
+	const BASE_URL = 'https://records-ws.nbnatlas.org/';
+
+	/**
+	 * The unique data resource id code
+	 *
+	 * The id can by found by searching the NBN Atlas data sets at
+	 * https://registry.nbnatlas.org/datasets. The id is located in the URL of a
+	 * data resource page and consists of the letters "dr" followed by a number;
+	 * e.g., https://registry.nbnatlas.org/public/showDataResource/dr782
+	 *
+	 * dr782 is the SEDN data set.
+	 *
+	 * Use dr1323 for Worcestershire data if SEDN data not available:
+	 * https://registry.nbnatlas.org/public/showDataResource/dr1323
+	 *
+	 * @var string $dataResourceUid
+	 */
+	private $dataResourceUid = 'dr1323';
+
+	/**
+	 * TODO: Describe what the $path member variable is for
+	 *
+	 * @var string $path
+	 */
+	private $path = '';
+
+	/**
+	 * TODO: Describe what the $facets member variable is for
+	 *
+	 * @var string $facets
+	 */
 	public $facets;
+
+	/**
+	 * TODO: Describe what the $fsort member variable is for
+	 *
+	 * @var string $fsort
+	 */
 	public $fsort;
+
+	/**
+	 * TODO: Describe what the $pageSize member variable is for
+	 *
+	 * @var integer $pageSize
+	 */
 	public $pageSize = 9;
+
+	/**
+	 * TODO: Describe what the $sort member variable is for
+	 *
+	 * @var string $sort
+	 */
 	public $sort;
 
-	public function __construct($path = 'occurrences/search')
+	/**
+	 * Constructor
+	 *
+	 * Accepts a path fragment which indicates the NBN Atlas API search type to
+	 * perform. Defaults to Occurrence search: https://api.nbnatlas.org/#ws3
+	 *
+	 * See https://api.nbnatlas.org/ for others.
+	 *
+	 * @param string $path NBN Atlas API search type
+	 */
+	public function __construct(string $path = 'occurrences/search')
 	{
 		$this->path = $path;
 	}
@@ -22,9 +85,11 @@ class NbnRecords
 	/**
 	 * Return the base search query string
 	 *
+	 * @param string $url The full url to query
+	 *
 	 * @return string
 	 */
-	private function getQueryString($url)
+	private function getQueryString(string $url)
 	{
 		$queryString  = $url . '?';
 		$queryString .= 'q=data_resource_uid:' . $this->dataResourceUid . '&';
@@ -36,7 +101,10 @@ class NbnRecords
 	}
 
 	/**
-	 * Return the base url and path (really only used for getting a single occurence record)
+	 * Return the base url and path (really only used for getting a single
+	 * occurence record)
+	 *
+	 * @return string
 	 */
 	protected function url()
 	{
@@ -68,19 +136,28 @@ class NbnRecords
 	}
 
 	/**
-	 * @var string[]
+	 * Keeps an internal array of query filter parameters.
+	 *
+	 * A list of available index fields can be found at
+	 * https://species-ws.nbnatlas.org/admin/indexFields
+	 *
+	 * @var string[] Array of strings
 	 */
 	protected $filterQueryParameters = [];
 
 	/**
 	 * Adds to the internal list of filter query parameters
 	 *
+	 * A list of available index fields can be found at
+	 * https://species-ws.nbnatlas.org/admin/indexFields
+	 *
+	 * @param string $filterQueryParameter A single filter query parameter
+	 *
 	 * @return $this
 	 */
-	public function add($filterQueryParameter)
+	public function add(string $filterQueryParameter)
 	{
 		$this->filterQueryParameters[] = $filterQueryParameter;
 		return $this;
 	}
-
 }
