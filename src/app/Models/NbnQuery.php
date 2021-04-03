b3b427f2bd3025e22c02674345a2e757f85643fe
Charles Roper <charles@roper.im>
2021-04-03 04:49:50 +0100

    Apply minor formatting fixes

--- src/app/Models/NbnQuery.php
+++ src/app/Models/NbnQuery.php
@@ -24,26 +24,26 @@ class NbnQuery implements NbnQueryInterface
 		//because the API respects the case
 		$name_search_string = ucfirst($name_search_string);
 
-		if ($speciesGroup=="both")
+		if ($speciesGroup === "both")
 		{
-			$speciesGroup='Plants+Bryophytes';
+			$speciesGroup = 'Plants+Bryophytes';
 		}
 		else
 		{
-			$speciesGroup=ucfirst($speciesGroup);
+			$speciesGroup = ucfirst($speciesGroup);
 		}
-		$nbn_records        = new NbnRecords('explore/group/ALL_SPECIES');
+		$nbn_records = new NbnRecords('explore/group/ALL_SPECIES');
 		$nbn_records
-			->add('taxon_name:' . str_replace(" ", "+%2B",$name_search_string) . '*')
+			->add('taxon_name:' . str_replace(" ", "+%2B", $name_search_string) . '*')
 			->add('species_group:' . $speciesGroup)
 		;
 		$query_url         = $nbn_records->getPagingQueryString();
 		$species_list_json = file_get_contents($query_url);
 		$species_list      = json_decode($species_list_json);
 
-		$speciesQueryResult=new NbnQueryResult();
-		$speciesQueryResult->records=$species_list;
-		$speciesQueryResult->downloadLink=$nbn_records->getDownloadQueryString();
+		$speciesQueryResult               = new NbnQueryResult();
+		$speciesQueryResult->records      = $species_list;
+		$speciesQueryResult->downloadLink = $nbn_records->getDownloadQueryString();
 		return $speciesQueryResult;
 	}
 
@@ -69,7 +69,7 @@ class NbnQuery implements NbnQueryInterface
 		$record_list  = json_decode($records_json)->occurrences;
 		usort($record_list, function ($a, $b) {
 			return $b->year <=> $a->year;
-		  });
+		});
 		$records['download_link'] = $nbn_records->getDownloadQueryString();
 
 		foreach ($record_list as $record)
