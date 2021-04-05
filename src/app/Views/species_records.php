5ed811ad015ade6c9194935ce24f2540145e6d59
Charles Roper <charles@roper.im>
2021-04-05 01:37:48 +0100

    Minor formatting improvements

--- src/app/Views/species_records.php
+++ src/app/Views/species_records.php
@@ -7,7 +7,10 @@
 			<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
 		</svg>
 	</a>
-	<h2><?= urldecode($species_name) ?> records in <?= urldecode($site_name) ?></h2>
+	<h2>
+		<?= urldecode($species_name) ?> records in
+		<?= urldecode($site_name) ?>
+	</h2>
 </div>
 <?php if (isset($download_link)) : ?>
 	<p><a href="<?= $download_link ?>">Download this data</a></p>
@@ -44,12 +47,26 @@
 					<?php foreach ($records_list as $record) : ?>
 						<tr>
 							<td>
-								<a href="/site/<?= $record->locationId ?>/group/plants/type/scientific"><?= $record->locationId ?></a>
+								<a href="/site/<?= $record->locationId ?>/group/plants/type/scientific">
+									<?= $record->locationId ?>
+								</a>
 							</td>
-							<td><a href="/square/<?= $record->gridReference ?>/group/plants/type/scientific"><?= $record->gridReference ?></a></td>
-							<td><?= $record->collector ?></td>
-							<td><?= $record->year ?></td>
-							<td><a href="<?= base_url("/record/{$record->uuid}"); ?>">more</td>
+							<td>
+								<a href="/square/<?= $record->gridReference ?>/group/plants/type/scientific">
+									<?= $record->gridReference ?>
+								</a>
+							</td>
+							<td>
+								<?= $record->collector ?>
+							</td>
+							<td>
+								<?= $record->year ?>
+							</td>
+							<td>
+								<a href="<?= base_url(" /record/{$record->uuid}"); ?>">
+									more
+								</a>
+							</td>
 						</tr>
 					<?php endforeach; ?>
 				</tbody>
@@ -86,13 +103,12 @@
 		+ "gridlabels:true;gridres:singlegrid"
 		+ "&fq=data_resource_uid:dr782";
 
-	const species = L.tileLayer.wms(
-		wmsUrl, {
-			"layers": "ALA:occurrences",
-			"uppercase": true,
-			"format": "image/png",
-			"transparent": true
-		});
+	const species = L.tileLayer.wms(wmsUrl, {
+		"layers": "ALA:occurrences",
+		"uppercase": true,
+		"format": "image/png",
+		"transparent": true
+	});
 	//make a map and add the layers
 	const map = L.map("map", {
 		center: [52.6, -3.0],
