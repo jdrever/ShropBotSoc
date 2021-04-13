a50344d4e44947fc5a7915a8db6bb5cfd82efe45
James Drever <james@careful.digital>
2021-04-13 09:11:55 +0100

    application of coding standards

--- src/app/Models/NbnQueryCached.php
+++ src/app/Models/NbnQueryCached.php
@@ -8,49 +8,64 @@ class NbnQueryCached implements NbnQueryInterface
 	/**
 	 * Determines whether caching is active
 	 * Set to false on development environments
-	 * @var bool
+	 *
+	 * @var    bool
 	 * @access private
+	 *
+	 * @TODO: refactoring to implement DRY with repeated caching code
 	 */
-	private const CACHE_ACTIVE=true;
+	private const CACHE_ACTIVE = true;
 
-	function __construct()
+	/**
+	 * Constructor, initiliases NbnQuery
+	 *
+	 * @access public
+	 */
+	public function __construct()
 	{
 		$this->nbnQuery = model('App\Models\NbnQuery', false);
-
 	}
 
 	/**
 	 * Cache the search for species within the county
 	 *
-	 * @return
+	 * @param [string] $nameSearchString the species name
+	 * @param [string] $nameType         scientific or common
+	 * @param [string] $speciesGroup     plants, bryophytes or both
+	 *
+	 * @return nbnQueryResult
 	 */
-	public function getSpeciesListForCounty($name_search_string, $name_type, $species_group)
+	public function getSpeciesListForCounty($nameSearchString, $nameType, $speciesGroup)
 	{
-		$name_search_string = ucfirst($name_search_string); //because the API respects the case
-		$cache_name         = "get-species-list-for-county-$name_type-$species_group-$name_search_string";
-		if (! self::CACHE_ACTIVE || ! $species_list = cache($cache_name))
+		$nameSearchString = ucfirst($nameSearchString); //because the API respects the case
+		$cacheName         = "get-species-list-for-county-$nameType-$speciesGroup-$nameSearchString";
+		if (! self::CACHE_ACTIVE || ! $speciesList = cache($cacheName))
 		{
-			$species_list = $this->nbnQuery->getSpeciesListForCounty($name_search_string, $name_type, $species_group);
+			$speciesList = $this->nbnQuery->getSpeciesListForCounty($nameSearchString, $nameType, $speciesGroup);
 			if (self::CACHE_ACTIVE)
 			{
-				cache()->save($cache_name, $species_list, CACHE_LIFE);
+				cache()->save($cacheName, $speciesList, CACHE_LIFE);
 			}
 		}
-		return $species_list;
+		return $speciesList;
 	}
 
 	/**
 	 * Cache the single species search within the county
+	 *
+	 * @param [string] $speciesName the name of the species
+	 *
+	 * @return nbnQueryResult
 	 */
-	public function getSingleSpeciesRecordsForCounty($species_name)
+	public function getSingleSpeciesRecordsForCounty($speciesName)
 	{
-		$cache_name= "get-single-species-for-county $species_name";
-		if (! self::CACHE_ACTIVE || ! $speciesRecords = cache($cache_name))
+		$cacheName = "get-single-species-for-county-$speciesName";
+		if (! self::CACHE_ACTIVE || ! $speciesRecords = cache($cacheName))
 		{
-			$speciesRecords=$this->nbnQuery->getSingleSpeciesRecordsForCounty($species_name);
+			$speciesRecords = $this->nbnQuery->getSingleSpeciesRecordsForCounty($speciesName);
 			if (self::CACHE_ACTIVE)
 			{
-				cache()->save($cache_name, $speciesRecords, CACHE_LIFE);
+				cache()->save($cacheName, $speciesRecords, CACHE_LIFE);
 			}
 		}
 		return $speciesRecords;
@@ -58,67 +73,111 @@ class NbnQueryCached implements NbnQueryInterface
 
 	/**
 	 * Cache the single occurence record
+	 *
+	 * @param [string] $uuid the unique id for the occurence
+	 *
+	 * @return [nbnQueryResult]
 	 */
 	public function getSingleOccurenceRecord($uuid)
 	{
-		$cache_name         = "get-single-occurence-record-$uuid";
-		if (! self::CACHE_ACTIVE || ! $occurenceRecord = cache($cache_name))
+		$cacheName = "get-single-occurence-record-$uuid";
+		if (! self::CACHE_ACTIVE || ! $occurenceRecord = cache($cacheName))
 		{
-			$occurenceRecord=$this->nbnQuery->getSingleOccurenceRecord($uuid);
+			$occurenceRecord = $this->nbnQuery->getSingleOccurenceRecord($uuid);
 			if (self::CACHE_ACTIVE)
 			{
-				cache()->save($cache_name, $occurenceRecord, CACHE_LIFE);
+				cache()->save($cacheName, $occurenceRecord, CACHE_LIFE);
 			}
+		}
 		return $occurenceRecord;
 	}
 
 	/**
 	 * Cache the site list for the county
+	 *
+	 * @param [string] $siteSearchString the name of the site
+	 *
+	 * @return [nbnQueryResult]
 	 */
-	public function getSiteListForCounty($site_search_string)
+	public function getSiteListForCounty($siteSearchString)
 	{
-		$name_search_string = ucfirst($site_search_string);
-		$cache_name         = "get-site-list-for-county-$name_search_string";
-		if (! self::CACHE_ACTIVE || ! $site_list = cache($cache_name))
+		$nameSearchString = ucfirst($siteSearchString);
+		$cacheName         = "get-site-list-for-county-$nameSearchString";
+		if (! self::CACHE_ACTIVE || ! $siteList = cache($cacheName))
 		{
-			$site_list = $this->nbnQuery->getSiteListForCounty($site_search_string);
+			$siteList = $this->nbnQuery->getSiteListForCounty($nameSearchString);
 			if (self::CACHE_ACTIVE)
 			{
-				cache()->save($cache_name, $site_list, CACHE_LIFE);
+				cache()->save($cacheName, $siteList, CACHE_LIFE);
 			}
 		}
-		return $site_list;
+		return $siteList;
+	}
+
+	/**
+	 * Cache for site speciest list
+	 *
+	 * @param [string] $sitename     the name of the site
+	 * @param [string] $speciesGroup plants, bryophytes or both
+	 *
+	 * @return nbnQueryResult
+	 */
+	public function getSpeciesListForSite($siteName, $speciesGroup)
+	{
+		$cacheName = "get-species-list-for-site-$speciesGroup";
+		if (! self::CACHE_ACTIVE || ! $speciesList = cache($cacheName))
+		{
+			$speciesList = $this->nbnQuery->getSpeciesListForSite($siteName, $speciesGroup);
+			if (self::CACHE_ACTIVE)
+			{
+				 cache()->save($cacheName, $speciesList, CACHE_LIFE);
+			}
+		}
+		return $speciesList;
+	}
+
+	/**
+	 * Cache the single species record for a site
+	 *
+	 * @param [string] $siteName    the site name
+	 * @param [string] $speciesName the species name
+	 *
+	 * @return [nbnQueryResult]
+	 */
+	public function getSingleSpeciesRecordsForSite($siteName, $speciesName)
+	{
+		$cacheName = "get-species-records-for-site-$siteName-$speciesName";
+		if (! self::CACHE_ACTIVE || ! $speciesList = cache($cacheName))
+		{
+			$speciesRecords = $this->nbnQuery->getSingleSpeciesRecordsForSite($siteName, $speciesName);
+			if (self::CACHE_ACTIVE)
+			{
+				 cache()->save($cacheName, $speciesRecords, CACHE_LIFE);
+			}
+		}
+		return $speciesRecords
+	}
+
+	/**
+	 * Cache species list for square
+	 *
+	 * @param [string] $gridSquare  the grid square
+	 * @param [string] $speciesGroup plants, bryophytes or both
+	 *
+	 * @return [nbnQueryResult]
+	 *
+	 * @TODO: implement speciesGroup filtering
+	 */
+	public function getSpeciesListForSquare($gridSquare, $speciesGroup)
+	{
+		return $this->nbnQuery->getSpeciesListForSquare($gridSquare);
 	}
 
 	/**
 	 * TODO: caching
 	 */
-	public function getSpeciesListForSite($site_name, $species_group)
+	public function getSingleSpeciesRecordsForSquare($grid_square, $speciesName)
 	{
-		return $this->nbnQuery->getSpeciesListForSite($site_name, $species_group);
-	}
-
-	/**
-	 * TODO: caching
-	 */
-	public function getSingleSpeciesRecordsForSite($site_name, $species_name)
-	{
-		return $this->nbnQuery->getSingleSpeciesRecordsForSite($site_name, $species_name);
-	}
-
-	/**
-	 * TODO: caching
-	 */
-	public function getSpeciesListForSquare($grid_square, $species_group)
-	{
-		return $this->nbnQuery->getSpeciesListForSquare($grid_square);
-	}
-
-	/**
-	 * TODO: caching
-	 */
-	public function getSingleSpeciesRecordsForSquare($grid_square, $species_name)
-	{
-		return $this->nbnQuery->getSingleSpeciesRecordsForSquare($grid_square, $species_name);
+		return $this->nbnQuery->getSingleSpeciesRecordsForSquare($grid_square, $speciesName);
 	}
 }
