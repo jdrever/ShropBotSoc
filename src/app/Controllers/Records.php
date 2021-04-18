d8b4af05f98d747e6e420ba69bf9d99f11316a15
Charles Roper <charles@roper.im>
2021-04-18 03:07:22 +0100

    Auto format Records.php

--- src/app/Controllers/Records.php
+++ src/app/Controllers/Records.php
@@ -1,64 +1,66 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 /**
  * Manage the records views.
  */
 class Records extends BaseController
 {
-    private $data = array('title' => 'Records');
+	private $data = ['title' => 'Records'];
 
-    /**
-     * List the records for a species in the entire dataset
-     */
-    public function singleSpeciesForCounty($speciesName)
-    {
-        $this->data['site_name'] = "Shropshire";
-		$this->data['title'] = urldecode($speciesName);
+	/**
+	 * List the records for a species in the entire dataset
+	 */
+	public function singleSpeciesForCounty($speciesName)
+	{
+		$this->data['site_name'] = "Shropshire";
+		$this->data['title']     = urldecode($speciesName);
 		if (isset($_GET['name']))
 		{
 			$this->data['speciesName'] = $_GET['name'];
 		}
 		else
 		{
-        	$this->data['speciesName'] = $speciesName;
+			$this->data['speciesName'] = $speciesName;
 		}
-		$records = $this->nbn->getSingleSpeciesRecordsForCounty($speciesName,$this->page);
-        $this->data['download_link'] = $records->downloadLink;
-        $this->data['recordsList']   = $records->records;
+		$records                     = $this->nbn->getSingleSpeciesRecordsForCounty($speciesName, $this->page);
+		$this->data['download_link'] = $records->downloadLink;
+		$this->data['recordsList']   = $records->records;
 		$this->data['sites']         = $records->sites;
 		$this->data['page']          = $this->page;
 		$this->data['queryUrl']      = $records->queryUrl;
 		$this->data['totalRecords']  = $records->totalRecords;
 		$this->data['totalPages']    = $records->getTotalPages();
-        echo view('species_records', $this->data);
-    }
+		echo view('species_records', $this->data);
+	}
 
-    /**
-     * Display records for a single species for a site
-     */
-    public function singleSpeciesForSite($site_name, $Nme)
-    {
-        // Map of site
-        $this->data['site_name'] = $site_name;
-        $this->data['Nme'] = $Nme;
-        $this->data['records_list'] = $this->nbn->getSingleSpeciesRecordsForSite($site_name, $Nme);
-        echo view('species_records', $this->data);
-    }
+	/**
+	 * Display records for a single species for a site
+	 */
+	public function singleSpeciesForSite($site_name, $Nme)
+	{
+		// Map of site
+		$this->data['site_name']    = $site_name;
+		$this->data['Nme']          = $Nme;
+		$this->data['records_list'] = $this->nbn->getSingleSpeciesRecordsForSite($site_name, $Nme);
+		echo view('species_records', $this->data);
+	}
 
-    /**
-     * Display a single record
-     */
-    public function singleRecord($uuid)
-    {
-        $record = $this->nbn->getSingleOccurenceRecord($uuid);
-        $occurrence = $record->processed->occurrence;
-        $this->data['occurrence'] = $occurrence;
-        $classification = $record->processed->classification;
-        $this->data['classification'] = $classification;
-        $title = $classification->scientificName."-".$occurrence->recordedBy;
-        $this->data['location'] = $record->raw->location; # `raw` contains the locationID
-        $this->data['event'] = $record->processed->event;
-        $this->data['title'] = $title;
-        echo view('single_record', $this->data);
-    }
+	/**
+	 * Display a single record
+	 */
+	public function singleRecord($uuid)
+	{
+		$record                       = $this->nbn->getSingleOccurenceRecord($uuid);
+		$occurrence                   = $record->processed->occurrence;
+		$this->data['occurrence']     = $occurrence;
+		$classification               = $record->processed->classification;
+		$this->data['classification'] = $classification;
+		$title                        = $classification->scientificName . "-" . $occurrence->recordedBy;
+		$this->data['location']       = $record->raw->location; # `raw` contains the locationID
+		$this->data['event']          = $record->processed->event;
+		$this->data['title']          = $title;
+		echo view('single_record', $this->data);
+	}
 }
