bf1f05d97542e9960a4bd7bbff62c15b020b473b
Charles Roper <charles@roper.im>
2021-04-30 05:01:03 +0100

    Get tabs and map working

--- src/app/Views/single_record.php
+++ src/app/Views/single_record.php
@@ -22,15 +22,15 @@
 
 	<ul id="tabs" class="nav nav-tabs d-lg-none" role="tablist">
 		<li class="nav-item" role="presentation">
-			<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
+			<button class="nav-link active" id="map-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
 		</li>
 		<li class="nav-item" role="presentation">
-			<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
+			<button class="nav-link" id="data-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
 		</li>
 	</ul>
 
 	<div id="tab-content" class="row">
-		<div id="data" class="tab-pane fade show col-lg">
+		<div id="data" class="tab-pane fade show active col-lg">
 			<table class="table table-sm">
 				<thead>
 					<tr>
@@ -74,46 +74,69 @@
 				</tbody>
 			</table>
 		</div>
-		<div id="map-container" class="tab-pane fade show active col-lg">
-			<div id="map" class="">Map not yet implemented</div>
+		<div id="map-container" class="tab-pane fade show col-lg">
+			<div id="map" class=""></div>
 		</div>
 	</div>
-	<!--
-<div id="mapid"></div>
-	<?= $location->gridReferenceWKT ?>
-<script>
-	var mymap = L.map("mapid").setView([53.0, -3.0], 13);
 
-	var osmLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
-		id: "mapbox/streets-v11",
-		tileSize: 512,
-		zoomOffset: -1,
-		accessToken: "pk.eyJ1Ijoiam9lamNvbGxpbnMiLCJhIjoiY2tnbnpjZmtpMGM2MTJ4czFqdHEzdmNhbSJ9.Fin7MSPizbCcQi6hSzVigw"
-	});
+	<script>
+		// Initialise the map
+		const map = L.map("map", {
+			zoomSnap: 0,
+		}).setView([52.6354, -2.71975], 13);
 
-	var britishGrid = L.britishGrid({
-		color: "#00f",
-		showSquareLabels: [100000], // label 100km grid squares
-		drawClip: true
-	});
 
-	var baseMaps = {
-		"OSM": osmLayer
-	};
+		// Make a minimal base layer using Mapbox data
+		const minimal = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 18,
+			id: "mapbox/outdoors-v11",
+			tileSize: 512,
+			zoomOffset: -1,
+			accessToken: "pk.eyJ1IjoiY2hhcmxlc3JvcGVyIiwiYSI6ImNrbmY2YXl4ZTJjbDQydm1xOW83MXh1eDIifQ.ntclZm-a8OxwUEBODW08FQ"
+		});
 
-	var overlayMaps = {
-		"British Grid": britishGrid
-	};
+		// Initialise geoJson boundary layer
+		const boundary = L.geoJSON(null, {
+			"color": "#0996DB",
+			"weight": 5,
+			"opacity": 0.33
+		});
 
-	//L.control.layers(baseMaps, overlayMaps).addTo(mymap);
-	osmLayer.addTo(mymap);
+		// Create a Layer Group and add to map
+		const layers = L.layerGroup([minimal, boundary]);
+		layers.addTo(map);
 
-	var options = {};
-	L.osGraticule(options).addTo(mymap);
-</script>
 
--->
+
+		// We load the geojson data from disk using the JavaScript Fetch API. When
+		// the response resolves, we add the data to the boundary layer and use the
+		// fitBounds() Leaflet method to zoom and position the map around the
+		// boundary data with a touch of padding.
+		const url = "/data/shropshire_simple.json";
+		fetch(url)
+			.then((response) => response.json())
+			.then((geojson) => {
+				boundary.addData(geojson);
+				map.fitBounds(boundary.getBounds(geojson).pad(0.1));
+			});
+
+
+		["load", "resize"].forEach((event) => {
+			window.addEventListener(event, () => {
+				const activeTab = document.querySelector("[aria-selected='true']");
+				new bootstrap.Tab(activeTab).show();
+
+				if (window.matchMedia("(min-width: 992px)").matches) {
+					document.querySelector("#tab-content").classList.remove("tab-content");
+					document.querySelector("#map-container").classList.add("show");
+					document.querySelector("#data").classList.add("show");
+				} else {
+					document.querySelector("#tab-content").classList.add("tab-content");
+					bootstrap.Tab.getInstance(activeTab).show();
+				}
+			});
+		});
+	</script>
 <?php endif ?>
 <?= $this->endSection() ?>
