bee725a31df36e66bf49e803ce04fb71fad7276d
jdrever <james@careful.digital>
2021-09-03 15:05:08 +0100

    trying use string for samesite not constant

--- src/app/Controllers/Species.php
+++ src/app/Controllers/Species.php
@@ -1,7 +1,5 @@
 <?php namespace App\Controllers;
 
-use CodeIgniter\Cookie\Cookie;
-use DateTime;
 /**
  * Manage the species or taxa views.
  */
@@ -73,7 +71,7 @@ class Species extends BaseController
 		$this->data['totalPages']       = $speciesQueryResult->getTotalPages();
 
 
-		set_cookie("speciesNameSearch",$name_search_string,"3600", "", "/", "", false, false, Cookie::SAMESITE_STRICT);
+		set_cookie("speciesNameSearch",$name_search_string,"3600", "", "/", "", false, false, "strict");
 		set_cookie("nameType", $name_type, "3600", "", "/", "", false, false, null);
 		set_cookie("speciesGroup", $species_group, "3600", "", "/", "", false, false, null);
 
