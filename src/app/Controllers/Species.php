4825806e126e64d900b19007cb3a4213895f6731
jdrever <james@careful.digital>
2021-09-03 14:40:03 +0100

    testing set domain to empty string in set_cookie

--- src/app/Controllers/Species.php
+++ src/app/Controllers/Species.php
@@ -70,7 +70,7 @@ class Species extends BaseController
 		$this->data['totalRecords'] = $speciesQueryResult->totalRecords;
 		$this->data['totalPages']       = $speciesQueryResult->getTotalPages();
 
-		set_cookie("speciesNameSearch",$name_search_string);//,"3600", "localhost", "/", "", false, false, null);
+		set_cookie("speciesNameSearch",$name_search_string,"3600", "", "/", "", false, false, null);
 		set_cookie("nameType", $name_type);//, "3600", "localhost", "/", "", false, false, null);
 		set_cookie("speciesGroup", $species_group);//, "3600", "localhost", "/", "", false, false, null);
 
