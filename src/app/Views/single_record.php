a1e5fd0eec1ac461f49b9385999d534dced43720
jdrever <james@careful.digital>
2021-10-25 15:39:38 +0100

    adding bold to tab items

--- src/app/Views/single_record.php
+++ src/app/Views/single_record.php
@@ -22,10 +22,10 @@
 
 	<ul id="tabs" class="nav nav-tabs d-lg-none" role="tablist">
 		<li class="nav-item" role="presentation">
-			<button class="nav-link active" id="map-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
+			<button class="nav-link active fw-bold" id="map-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
 		</li>
 		<li class="nav-item" role="presentation">
-			<button class="nav-link" id="data-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
+			<button class="nav-link fw-bold" id="data-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
 		</li>
 	</ul>
 
--- src/app/Views/species_records.php
+++ src/app/Views/species_records.php
@@ -28,10 +28,10 @@
 
 <ul id="tabs" class="nav nav-tabs d-lg-none" role="tablist">
 	<li class="nav-item" role="presentation">
-		<button class="nav-link active" id="profile-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
+		<button class="nav-link active fw-bold" id="profile-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="home-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
+		<button class="nav-link fw-bold" id="home-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
 	</li>
 </ul>
 <p><?= $totalRecords ?> records</p>
