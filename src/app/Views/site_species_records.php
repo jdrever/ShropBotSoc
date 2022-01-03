fcf4b6796792d1e60a51d5aa21b5a548857649e5
jdrever <james@careful.digital>
2022-01-03 14:37:13 +0000

    adding pagination/removing under development

--- src/app/Views/site_species_records.php
+++ src/app/Views/site_species_records.php
@@ -12,9 +12,7 @@
 	</h2>
 </div>
 
-<div class="alert alert-info" role="alert">
-	PLEASE NOTE: this page is currently still under development and may not return accurate information.
-</div>
+
 
 <?php if (isset($message)) : ?>
 	<div class="alert alert-danger" role="alert">
@@ -29,24 +27,18 @@
 
 <ul id="tabs" class="nav nav-tabs d-lg-none" role="tablist">
 	<li class="nav-item" role="presentation">
-		<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="true">Map</button>
+		<button class="nav-link active" id="profile-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="true">Data</button>
 	</li>
 	<li class="nav-item" role="presentation">
-		<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#data" type="button" role="tab" aria-controls="data" aria-selected="false">Data</button>
+		<button class="nav-link" id="home-tab" data-bs-toggle="tab" data-bs-target="#map-container" type="button" role="tab" aria-controls="map" aria-selected="false">Map</button>
 	</li>
 </ul>
 <div id="tab-content" class="row">
-
-	<div id="map-container" class="tab-pane fade show active col-lg">
-		<div id="map" class="">Map not yet developed</div>
-	</div>
-
-	<div id="data" class="tab-pane fade show col-lg">
+	<div id="data" class="tab-pane fade show active col-lg">
 		<?php if (isset($recordsList)) : ?>
 			<table class="table">
 				<thead>
 					<tr>
-						<th>Site</th>
 						<th class="d-none d-sm-table-cell">Square</th>
 						<th class="d-none d-md-table-cell">Collector</th>
 						<th>Year</th>
@@ -56,11 +48,6 @@
 				<tbody>
 					<?php foreach ($recordsList as $record) : ?>
 						<tr data-uuid="<?= $record->uuid ?>">
-							<td>
-								<a href="/site/<?= $record->locationId ?>/species/<?=$speciesName ?>">
-									<?= $record->locationId ?>
-								</a>
-							</td>
 							<td class="d-none d-sm-table-cell">
 								<a href="/square/<?= $record->gridReference ?>/species/<?=$speciesName ?>">
 									<?= $record->gridReference ?>
@@ -83,17 +70,78 @@
 			</table>
 		<?php endif ?>
 	</div>
+	<div id="map-container" class="tab-pane fade show col-lg">
+			<div id="map" class=""></div>
+	</div>
+
+	<script>
+		// Initialise the map
+		const map = L.map("map", {
+			zoomSnap: 0,
+		}).setView([52.6354, -2.71975], 13);
+
+		// Make a minimal base layer using Mapbox data
+		const minimal = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 18,
+			id: "mapbox/outdoors-v11",
+			tileSize: 512,
+			zoomOffset: -1,
+			accessToken: "pk.eyJ1IjoiY2hhcmxlc3JvcGVyIiwiYSI6ImNrbmY2YXl4ZTJjbDQydm1xOW83MXh1eDIifQ.ntclZm-a8OxwUEBODW08FQ"
+		});
+
+		// Initialise geoJson boundary layer
+		const boundary = L.geoJSON(null, {
+			color: "#0996DB",
+			weight: 5,
+			opacity: 0.33
+		});
+
+		// Create a Layer Group and add to map
+		const layers = L.layerGroup([minimal, boundary]);
+
+		// Unless the first occurrence didn't contain a site location, create a
+		// marker for the site's location
+		<?php if (!empty($siteLocation)) : ?>
+		const siteMarker = L.marker(<?= json_encode($siteLocation) ?>, {
+			opacity: 0.75
+		});
+		layers.addLayer(siteMarker);
+		<?php endif ?>
+
+		layers.addTo(map);
+
+		// We load the geojson data from disk using the JavaScript Fetch API. When
+		// the response resolves, we add the data to the boundary layer and use the
+		// fitBounds() Leaflet method to zoom and position the map around the
+		// boundary data with a touch of padding.
+		const url = "/data/shropshire_simple.json";
+		fetch(url)
+			.then((response) => response.json())
+			.then((geojson) => {
+				boundary.addData(geojson);
+				map.fitBounds(boundary.getBounds(geojson).pad(0.1));
+			});
+
+		["load", "resize"].forEach((event) => {
+			window.addEventListener(event, () => {
+				const activeTab = document.querySelector("[aria-selected='true']");
+				new bootstrap.Tab(activeTab).show();
+
+				if (window.matchMedia("(min-width: 992px)").matches) {
+					document.querySelector("#tab-content").classList.remove("tab-content");
+					document.querySelector("#map-container").classList.add("show");
+					document.querySelector("#data").classList.add("show");
+				} else {
+					document.querySelector("#tab-content").classList.add("tab-content");
+					bootstrap.Tab.getInstance(activeTab).show();
+				}
+			});
+		});
+	</script>
+
 </div>
-<nav>
-	<ul class="pagination justify-content-center">
-		<?php if ($page > 1) : ?>
-			<li class="page-item"><a class="page-link" href="<?= current_url() . '?page=' . ($page - 1) ?>">Previous</a></li>
-		<?php endif ?>
-		<?php if (count($recordsList) === 10) : ?>
-			<li class="page-item"><a class="page-link" href="<?= current_url() . '?page=' . ($page + 1) ?>">Next</a></li>
-		<?php endif ?>
-	</ul>
-</nav>
+<?= $this->include('pagination') ?>
 
 <?php if (isset($download_link)) : ?>
 	<p><a href="<?= $download_link ?>">Download this data</a></p>
