09349953863f93f103f5faa4968ac8a604118c36
Tom Drever <tomdr1001@outlook.com>
2022-01-26 11:55:56 +0000

    Show map with outlined square on records for a single species in a square page

--- src/app/Views/square_species_records.php
+++ src/app/Views/square_species_records.php
@@ -1,5 +1,13 @@
 <?= $this->extend('default') ?>
 <?= $this->section('content') ?>
+<?php if (isset($message)) : ?>
+<div class="alert alert-danger" role="alert">
+	I am very sorry, but an error has occured.</b>:  <?= $message ?>
+</div>
+<?php endif ?>
+
+<?php if ($status ==='OK') : ?>
+
 <div class="d-flex align-items-center">
 	<a href="javascript:history.back()" class="header-backArrow">
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
@@ -11,16 +19,6 @@
 		<?= urldecode($gridSquare) ?>
 	</h2>
 </div>
-
-<div class="alert alert-info" role="alert">
-	PLEASE NOTE: this page is currently still under development and may not return accurate information.
-</div>
-
-<?php if (isset($message)) : ?>
-<div class="alert alert-danger" role="alert">
-	I am very sorry, but an error has occured.</b>:  <?= $message ?>
-</div>
-<?php else: ?>
 
 
 
@@ -37,11 +35,6 @@
 	</li>
 </ul>
 <div id="tab-content" class="row">
-
-	<div id="map-container" class="tab-pane fade show active col-lg">
-		<div id="map" class="">Map not yet developed</div>
-	</div>
-
 	<div id="data" class="tab-pane fade show col-lg">
 		<?php if (isset($recordsList)) : ?>
 			<table class="table">
@@ -84,17 +77,99 @@
 			</table>
 		<?php endif ?>
 	</div>
+	<div id="map-container" class="tab-pane fade show active col-lg">
+		<div id="map" class=""></div>
+	</div>
 </div>
-<nav>
-	<ul class="pagination justify-content-center">
-		<?php if ($page > 1) : ?>
-			<li class="page-item"><a class="page-link" href="<?= current_url() . '?page=' . ($page - 1) ?>">Previous</a></li>
-		<?php endif ?>
-		<?php if (count($recordsList) === 10) : ?>
-			<li class="page-item"><a class="page-link" href="<?= current_url() . '?page=' . ($page + 1) ?>">Next</a></li>
-		<?php endif ?>
-	</ul>
-</nav>
+<script>
+	// Initialise the map
+	const map = L.map("map", {
+		zoomSnap: 0,
+	}).setView([52.6354, -2.71975], 9);
+
+	// Make a minimal base layer using Mapbox data
+	const minimal = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: "mapbox/outdoors-v11",
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: "pk.eyJ1Ijoiam9lamNvbGxpbnMiLCJhIjoiY2tnbnpjZmtpMGM2MTJ4czFqdHEzdmNhbSJ9.Fin7MSPizbCcQi6hSzVigw"
+	});
+
+	const wmsUrl = "https://records-ws.nbnatlas.org/mapping/wms/reflect?" +
+		"Q=<?= $speciesName ?>" +
+		"&ENV=colourmode:osgrid;gridres:singlegrid;color:ff0000;size:4;opacity:0.5" +
+		"&fq=data_resource_uid:dr782%20AND%20grid_ref_1000:<?= $gridSquare ?>";
+
+	const gridSquareTile = L.tileLayer.wms(wmsUrl, {
+		"layers": "ALA:occurrences",
+		"uppercase": true,
+		"format": "image/png",
+		"transparent": true
+	});
+
+	// OS Grid graticules
+	// 10km grid graticule shown between zoom levels 8 and 11 and has no axis labels
+	const graticule10km = L.britishGrid({
+		color: '#216fff',
+		weight: 1,
+		showAxisLabels: [],
+		minInterval: 10000,
+		maxInterval: 10000,
+		minZoom: 8,
+		maxZoom: 11
+	});
+
+	// 1km grid graticule shown between zoom levels 11 and 15 and has labelled axis
+	const graticule1km = L.britishGrid({
+		color: '#216fff',
+		weight: 1,
+		showAxisLabels: [1000],
+		minInterval: 1000,
+		maxInterval: 1000,
+		minZoom: 11,
+		maxZoom: 15
+	});
+
+	// Initialise geoJson boundary layer
+	const boundary = L.geoJSON(null, {
+		"color": "#0996DB",
+		"weight": 5,
+		"opacity": 0.33
+	});
+
+	// Create a Layer Group and add to map
+	const layers = L.layerGroup([minimal, gridSquareTile, graticule10km, graticule1km, boundary]);
+	layers.addTo(map);
+
+	// Load shropshire geojson and fit map to boundaries
+	const url = "/data/shropshire_simple.json";
+	fetch(url)
+		.then((response) => response.json())
+		.then((geojson) => {
+			boundary.addData(geojson);
+			map.fitBounds(boundary.getBounds(geojson).pad(0.1));
+		});
+
+	["load", "resize"].forEach((event) => {
+		window.addEventListener(event, () => {
+			const activeTab = document.querySelector("[aria-selected='true']");
+			new bootstrap.Tab(activeTab).show();
+
+			if (window.matchMedia("(min-width: 992px)").matches) {
+				document.querySelector("#tab-content").classList.remove("tab-content");
+				document.querySelector("#map-container").classList.add("show");
+				document.querySelector("#data").classList.add("show");
+			} else {
+				document.querySelector("#tab-content").classList.add("tab-content");
+				bootstrap.Tab.getInstance(activeTab).show();
+			}
+		});
+	});
+</script>
+
+<?= $this->include('pagination') ?>
 
 	<?php if (isset($download_link)) : ?>
 <p><a href="<?= $download_link ?>">Download this data</a></p>
