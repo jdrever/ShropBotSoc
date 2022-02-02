8909cc47afe63c1b969226d0e35cefefbaabf5f2
Tom Drever <tomdr1001@outlook.com>
2022-02-02 16:43:27 +0000

    Implement red outline of square in square species records

--- src/app/Views/square_species_records.php
+++ src/app/Views/square_species_records.php
@@ -1,6 +1,21 @@
 <?= $this->extend('default') ?>
 <?= $this->section('content') ?>
 
+<style>
+	svg {
+      position: relative;
+    }
+    .square {
+      fill: #000;
+      fill-opacity: .2;
+	  stroke: red;
+      stroke-width: 3px;
+	}
+</style>
+
+<script src="https://unpkg.com/brc-atlas-bigr/dist/bigr.min.umd.js"></script>
+<script src="https://d3js.org/d3.v5.min.js"></script>
+
 <?php if (isset($message)) : ?>
 <div class="alert alert-danger" role="alert">
 	I am very sorry, but an error has occured.</b>:  <?= $message ?>
@@ -84,8 +99,52 @@
 	// Initialise the map
 	const map = initialiseBasicMap()
 
-	// TODO - use bigr to outline square location
-	// (and optionally zoom to fit it)
+	// Use d3 and bigr to convert the gridSquare into a path that is rendered
+	// onto the map whenever it is zoomed to highlight the grid square
+	var svg = d3.select(map.getPanes().overlayPane).append("svg")
+    var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+    var transform = d3.geoTransform({point: projectPoint})
+    var path = d3.geoPath().projection(transform)
+    var ftrSquares, squares
+
+    map.on("zoomend", reset)
+
+    function reset() {
+		var ftrSquare = {
+      		type: 'Feature',
+      		geometry: bigr.getGjson("<?= $gridSquare ?>", 'wg', 'square')
+    	}
+
+    	squares = g.selectAll("path")
+      		.data([ftrSquare])
+
+    	squares.enter()
+      		.append("path")
+      		.attr("d", path)
+      		.attr("class", 'square')
+
+      	var bounds = path.bounds({
+        	type: "FeatureCollection",
+        	features: [ftrSquare]
+      	})
+
+      	var topLeft = bounds[0]
+      	var bottomRight = bounds[1]
+
+      	svg.attr("width", bottomRight[0] - topLeft[0])
+        	.attr("height", bottomRight[1] - topLeft[1])
+        	.style("left", topLeft[0] + "px")
+        	.style("top", topLeft[1] + "px")
+
+      	g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")")
+
+      	squares.attr("d", path)
+    }
+
+    function projectPoint(x, y) {
+    	var point = map.latLngToLayerPoint(new L.LatLng(y, x))
+		this.stream.point(point.x, point.y)
+    }
 </script>
 
 <?= $this->include('pagination') ?>
