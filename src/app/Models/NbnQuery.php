677c45c951d7fc3da70743673009b062404ff31c
James Drever <james@careful.digital>
2023-05-05 12:43:09 +0100

    adding check for 504 error from NBN

--- src/app/Models/NbnQuery.php
+++ src/app/Models/NbnQuery.php
@@ -575,7 +575,7 @@ class NbnQuery implements NbnQueryInterface
 			{
 				$errorMessage = '<b>It looks like there is a problem with the query.</b>  Here are the details: ' . $errorMessage;
 			}
-			if (strpos($errorMessage, '500') !== false||strpos($errorMessage, '503') !== false ||strpos($errorMessage, 'php_network_getaddresses') !== false||strpos($errorMessage, 'SSL') !== false||strpos($errorMessage, 'stream') !== false)
+			if (strpos($errorMessage, '500') !== false||strpos($errorMessage, '503') !== false || strpos($errorMessage, '504') !== false ||strpos($errorMessage, 'php_network_getaddresses') !== false||strpos($errorMessage, 'SSL') !== false||strpos($errorMessage, 'stream') !== false)
 			{
 				$errorMessage = '<b>It looks like there is a problem with the NBN API</b>.  Here are the details: ' . $errorMessage;
 			}
