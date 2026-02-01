41878bcdd609be509a509e205b83bcd7c2d07c86
James Drever <james@careful.digital>
2026-02-01 09:25:20 +0000

    Error with $record->collector now being an array (square species records)

--- src/app/Views/square_species_records.php
+++ src/app/Views/square_species_records.php
@@ -75,7 +75,7 @@
 								</a>
 							</td>
 							<td class="d-none d-md-table-cell">
-								<?= $record->collector ?>
+								<?= implode(',', $record->collector) ?>
 							</td>
 							<td>
 								<?= $record->year ?>
